32befe6d36dc63a4547813b636d17f8bf00c9f46
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-31 19:50:26 -0600

    🚨 fix: linter warnings

--- index.js
+++ index.js
@@ -28,29 +28,34 @@ const schema = buildSchema(`
  * Must be named same as query.
  */
 const resolvers = {
-	hello: () => 'Hello World'
+  hello: () => 'Hello World',
 }
 
-app.use('/api', graphqlHTTP({
-	schema: schema,
-	rootValue: resolvers,
-	graphiql: true,
-}))
+app.use(
+  '/api',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+)
 
 /**
  * Raise the server.
  */
 app.listen(port, () => {
-	console.log(`[platzi-dir] Server is listening at http://localhost:${port}/api`)
+  console.log(
+    `[platzi-dir] Server is listening at http://localhost:${port}/api`
+  )
 })
 
 /**
  * Execute `hello` query
  */
 graphql(schema, '{ hello }', resolvers)
-	.then((data) => {
-		console.dir(data)
-	})
-	.catch((error) => {
-		console.error(error)
-	})
+  .then((data) => {
+    console.dir(data)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
