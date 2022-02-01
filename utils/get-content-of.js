9c0b74873a9313d63d199aba14de6b1d06baa5dd
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-02-01 15:36:10 -0600

    ✨ feat(utils): create `get-content-of`

---
+++ utils/get-content-of.js
@@ -0,0 +1,17 @@
+import { join } from 'path'
+import { readFileSync } from 'fs'
+import { settings } from '../core'
+
+/**
+ * Get the content from file in the given path.
+ * @param {string} filePath - Path of the directory (from root).
+ * @returns {string}
+ */
+export const getContentOf = (filePath) => {
+  const basePath = settings.ROOT_DIR
+  console.log('DEBUG:', basePath)
+  const fullPath = join(basePath, filePath)
+  const content = readFileSync(fullPath, 'utf-8')
+
+  return content
+}
