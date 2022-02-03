3f34f81a58aa18bd58c6dcab5bbe6355c4ecb925
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-02-03 17:48:35 -0600

    🔧 chore(core): update application settings
    Add new properties based on environment variables.

--- core/settings.js
+++ core/settings.js
@@ -1,10 +1,38 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 /*
  * Class representing the application configuration.
  * @class
  */
 class Configuration {
   /**
-   * Obtain the root path.
+   * Obtain the application name.
+   * @returns {string} - The application name.
+   */
+  APP_NAME = process.env.APP_NAME || 'myApplication'
+
+  /**
+   * Obtain the application host.
+   * @returns {string} - The application host.
+   */
+  APP_HOST = process.env.APP_HOST || 'localhost'
+
+  /**
+   * Obtain the application port.
+   * @returns {number} - The application port.
+   */
+  APP_PORT = Number(process.env.APP_PORT) || 3000
+
+  /**
+   * Obtain the application environment.
+   * @returns {string} - The application environment.
+   */
+  APP_ENV = process.env.APP_ENV || 'development'
+
+  /**
+   * Find the root path.
    * @returns {string} - The root path for the application.
    */
   getRootDir() {
@@ -12,6 +40,10 @@ class Configuration {
     return __root
   }
 
+  /**
+   * Obtain the application root path.
+   * @returns {string} - The application root path.
+   */
   get ROOT_DIR() {
     return this.getRootDir()
   }
