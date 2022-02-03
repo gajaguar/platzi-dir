48f5d8d917e121777a6d2b75f4cd1d40c47b434e
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-02-03 17:51:47 -0600

    🚧 chore: update `index`
    Modify sever starup message according to application settings.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict'
 
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
-import { makeExecutableSchema } from '@graphql-tools/schema'
+import { settings } from './core'
 import { resolvers } from './lib/resolvers'
 import { getContentOf } from './utils'
 
@@ -36,10 +37,17 @@ app.use(
 )
 
 /**
+ * Build the start up message and show it.
+ * @returns {Function} - The start up message.
+ */
+const showStarupMessage = () => {
+  const { APP_NAME, APP_HOST, APP_PORT, APP_ENV } = settings
+  const message = `[${APP_NAME}] application is running on http://${APP_HOST}:${APP_PORT} in ${APP_ENV} mode`
+
+  return console.log(message)
+}
+
+/**
  * Raise the server.
  */
-app.listen(port, () => {
-  console.log(
-    `[platzi-dir] Server is listening at http://localhost:${port}/api`
-  )
-})
+app.listen(port, showStarupMessage)
