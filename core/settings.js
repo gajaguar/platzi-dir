ea361c6bc244e69a0de19037762ff99f8931335c
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-02-03 18:40:59 -0600

    🔧 chore(core): update application settings
    Add new properties based on database environment variables.

--- core/settings.js
+++ core/settings.js
@@ -32,6 +32,49 @@ class Configuration {
   APP_ENV = process.env.APP_ENV || 'development'
 
   /**
+   * Obtain the database user name.
+   * @returns {string} - The database user name.
+   * @private
+   */
+  #DB_USER = process.env.DB_USER || 'root'
+
+  /**
+   * Obtain the database password.
+   * @returns {string} - The database password.
+   * @private
+   */
+  #DB_PASSWORD = process.env.DB_PASSWORD || 'root'
+
+  /**
+   * Obtain the database name.
+   * @returns {string} - The database name.
+   * @private
+   */
+  #DB_NAME = process.env.DB_NAME || 'myDatabase'
+
+  /**
+   * Obtain the database host.
+   * @returns {string} - The database host.
+   * @private
+   */
+  #DB_HOST = process.env.DB_HOST || 'localhost'
+
+  /**
+   * Obtain the database port.
+   * @returns {number} - The database port.
+   * @private
+   */
+  #DB_PORT = Number(process.env.DB_PORT) || 57017
+
+  /**
+   * Obtain the database connection string.
+   * @returns {string} - The database connection string.
+   */
+  DB_CONNECTION_STRING = `mongodb://${this.#DB_USER}:${this.#DB_PASSWORD}@${
+    this.#DB_HOST
+  }:${this.#DB_PORT}/${this.#DB_NAME}`
+
+  /**
    * Find the root path.
    * @returns {string} - The root path for the application.
    */
