9a4587171ae222f5eaea5a9ed889ab2cbe1e79ec
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-02-03 22:42:00 -0600

    ⚡️ chore(database): improve `populateDatabase`

--- database/populate.js
+++ database/populate.js
@@ -6,12 +6,18 @@ import { settings } from '../core'
  * Add mocked data to the database.
  */
 export const populateDatabase = async () => {
-  const dbConnetion = await getDbConnection()
-  const collection = dbConnetion.collection('courses')
-  const options = { ordered: true }
-  const result = await collection.insertMany(courses, options)
-  const { APP_NAME: appName } = settings
-  console.log(
-    `[${appName}] ${result.insertedCount} courses were added to the database.`
-  )
+  const { APP_NAME } = settings
+  try {
+    const dbConnetion = await getDbConnection()
+    const collection = dbConnetion.collection('courses')
+    const options = { ordered: true }
+    const result = await collection.insertMany(courses, options)
+    console.log(
+      `[${APP_NAME}] ${result.insertedCount} courses were added to the database.`
+    )
+  } catch (error) {
+    console.error(`[${APP_NAME}] ${error.message}`)
+
+    throw error
+  }
 }
