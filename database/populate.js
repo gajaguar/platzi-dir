4ddd3f66c700afc29b311570bc687b50660a24cc
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-02-03 21:30:32 -0600

    🐛 fix(database): await for `populateDatabase`
    Resolve the promise on `insertMany` operation.

--- database/populate.js
+++ database/populate.js
@@ -9,7 +9,7 @@ export const populateDatabase = async () => {
   const dbConnetion = await getDbConnection()
   const collection = dbConnetion.collection('courses')
   const options = { ordered: true }
-  const result = collection.insertMany(courses, options)
+  const result = await collection.insertMany(courses, options)
   const { APP_NAME: appName } = settings
   console.log(
     `[${appName}] ${result.insertedCount} courses were added to the database.`
